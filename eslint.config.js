ce877e178ced12181daceeb5e8a0e3667fb14e65
sero-momo <kurofune0915@gmail.com>
2025-12-28 19:33:32 +0900

    ➕eslint/prettier追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,96 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const { defineConfig } = require("eslint/config");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const eslintPluginImport = require('eslint-plugin-import');
+const eslintPluginUnusedImports = require('eslint-plugin-unused-imports');
 
 module.exports = defineConfig([
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
       tseslint.configs.recommended,
       tseslint.configs.stylistic,
       angular.configs.tsRecommended,
+      eslintConfigPrettier,
+      eslintPluginPrettierRecommended
     ],
+    plugins: {
+      prettier: eslintPluginPrettier,
+      import: eslintPluginImport,
+      'unused-imports': eslintPluginUnusedImports
+    },
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase'
+        }
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case'
+        }
       ],
-    },
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Node.jsの組込モジュール
+            'external', // 外部モジュール
+            'internal', // 自作モジュール
+            'parent', // 親階層のファイル
+            'sibling', // 同階層のファイル
+            'index' // 同階層のindexファイル
+          ],
+          'newlines-between': 'always', // グループ間に空行を挿入する
+          // インポートをアルファベット順にソートする
+          alphabetize: {
+            order: 'asc', // 昇順でソート
+            caseInsensitive: true // 大文字小文字を区別しない
+          },
+          // 特定のパターンのインポートに対してグループを指定
+          pathGroups: [
+            {
+              pattern: '@angular**', // @angular で始まるモジュール
+              group: 'external', // external グループに分類
+              position: 'before' // グループの最前に配置
+            }
+          ]
+        }
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      '@typescript-eslint/no-unused-vars': 'off' // 二重にエラーが表示されるのでOFFにする
+    }
   },
   {
-    files: ["**/*.html"],
-    extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
-    ],
-    rules: {},
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {}
+  },
+  {
+    files: ['**/*.html'],
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          parser: 'angular'
+        }
+      ]
+    }
   }
 ]);
---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  endOfLine: 'auto'
+};
+
+module.exports = config;
