37e6cb811c42c09e2722a08969406caedd4e5a16
sero-momo <kurofune0915@gmail.com>
2025-12-29 01:05:58 +0900

    🔧SCSS導入

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,17 +3,21 @@
  * @type {import('stylelint').Config}
  */
 const config = {
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-prettier/recommended',
+    'stylelint-config-recommended-scss'
+  ],
   plugins: ['stylelint-order'],
   rules: {
     'no-empty-source': null, // 空のソースを許可
     'order/properties-alphabetical-order': true, // CSSプロパティをアルファベット順でソート
     'import-notation': 'string', // @importの表記を文字列に統一
-    'at-rule-no-unknown': [
+    'scss/at-rule-no-unknown': [
       // Tailwind CSS用
       true,
       {
-        ignoreAtRules: ['tailwind', 'apply', 'variants', 'layer', 'screen']
+        ignoreAtRules: ['tailwind', 'apply', 'variants', 'layer', 'screen', 'reference']
       }
     ]
   }
